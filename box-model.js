1d0f88cd501c28272533d3e8039115380a091bbe
AriadnaFrunze <126607044+AriadnaFrunze@users.noreply.github.com>
2023-07-25 20:57:12 +0300

    Update box-model.js

--- box-model.js
+++ box-model.js
@@ -1,16 +1,1 @@
-childWidth = 100
-childPadding = 10
-childBorder = 1
-childMargin = 20
 
-parentWidth = 110
-
-childSize = childWidth + ( childPadding + childBorder + childMargin ) * 2
-
-overflow = childSize > parentWidth
-
-out = childSize - parentWidth
-
-alert("horizontal child size" + childSize + "px")
-alert("child overflow" + overflow)
-alert("overflow with" + out + "px")
