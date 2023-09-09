d5ec28cddf5205301385ec78fb5cd81a1be8a23b
AriadnaFrunze <126607044+AriadnaFrunze@users.noreply.github.com>
2023-09-09 16:40:58 +0300

    Delete Basics/array/array directory

--- Basics/array/array/array-motivation.js
+++
@@ -1,8 +0,0 @@
-let slides = [
-    "images/2023/slide-1.jpg",
-    "images/2023/slide-2.jpg",
-    "images/2023/slide-3.jpg"
-]
-for(let i = 0; i < 3; i++) {
-    document.write(`<img src="${slides[i]}">`)
-}
